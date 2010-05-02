502e024ced46aa66dfd5463666181a5b72b33ef7
yyscamper <yyscamper@6475faff-d798-06dd-ae1c-bddb2970e36a>
2010-05-02 17:45:17 +0000

    基本可以使用了。还需要： （1）对直方图进行平滑。 （2）Sigma的值多考虑 （3）SIFT特征还未提取，只是找到了关键点。

---  lowesift/SIFT/main.cpp
+++  lowesift/SIFT/main.cpp
@@ -1,27 +1,64 @@
 
 #include "sift.h"
+#include <time.h>
 #include <stdio.h>
 
 using namespace std;
+void GetBatchSiftFeature(char** imgNameList, int num);
+void SaveImage(IplImage* img);
 
 int main()
 {
-	char* path = "book.jpg";
+	char* imgNameList[3];
+	imgNameList[0] = "food1.jpg";
+	imgNameList[1] = "food2.jpg";
+	imgNameList[2] = "food3.jpg";
+	GetBatchSiftFeature(imgNameList, 3);
+	return 0;
+}
+
+void GetBatchSiftFeature(char** imgNameList, int num)
+{
+	IplImage** siftImgArr = (IplImage**)calloc(num, sizeof(IplImage*));
+	assert(siftImgArr != NULL);
 	IplImage* img;
-	img = cvLoadImage(path, CV_LOAD_IMAGE_COLOR);
-	if(img == NULL){
-		printf("\nError: failed to load image from : \"%s\"", path);
+	SiftToolbox sift;
+	for(int i=0; i<num; i++){
+		img = cvLoadImage(imgNameList[i], CV_LOAD_IMAGE_COLOR);
+		if(img == NULL){
+			printf("\nError: failed to load image: \"%s\"", imgNameList[i]);
+			return;
+		}
+		sift.Process(img);
+		siftImgArr[i] = sift.PlotKeypoint(SIFT_PLOT_ECLLIPSE);
+	}
+	
+	char wndName[] = "SIFT Feature 1";
+	for(int i=0; i<num; i++){
+		cvNamedWindow(wndName);
+		cvShowImage(wndName, siftImgArr[i]);
+		wndName[13]++;
+	}
+	cvWaitKey(0);
+	for(int i=0; i<num; i++){
+		cvReleaseImage(&siftImgArr[i]);
+	}
+	free(siftImgArr);
+	cvReleaseImage(&img);
+	cvDestroyAllWindows();
+}
+
+void SaveImage(IplImage* img)
+{
+	//Save the image
+	char saveImageName[128];
+	time_t now;
+	now = time(NULL);
+	struct tm *tmt = localtime(&now); 
+	strftime(saveImageName, 128, "%m%d.%H%M%S.jpg", tmt);
+	if(!cvSaveImage(saveImageName, img)){
+		printf("Error: Failed to save image.");
+		system("pause");
 		exit(0);
 	}
-
-	SiftToolbox sift;
-	sift.Process(img);
-	IplImage* siftImg = sift.PlotKeypoint();
-	cvNamedWindow("SIFT Feature");
-	cvShowImage("SIFT Feature", siftImg);
-	cvWaitKey(0);
-	cvReleaseImage(&img);
-	cvReleaseImage(&siftImg);
-	cvDestroyAllWindows();
-	return 0;
 }
